ad4b7fec919e46796266627879c92c79f138acd2
Hemily Mendes <110635046+moniquemdes@users.noreply.github.com>
2025-10-15 18:03:36 -0300

    added obstacle management and collision checking
    Added obstacle spawning and collision detection.

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import pygame
+import sys
 from entidades.hercules import Hercules
 from entidades.chao import Chao
 from entidades.ceu import Ceu
-
+from entidades.obstaculos import Obstaculo 
 pygame.init()
 
 # Configurações da tela
@@ -12,15 +13,24 @@ pygame.display.set_caption("HerPULEs")
 
 clock = pygame.time.Clock()
 
-# Criar jogador
+#Criar jogador
 hercules = Hercules(pos=(100, 504))
-grupo = pygame.sprite.GroupSingle(hercules)
+grupo_jogador = pygame.sprite.GroupSingle(hercules)
+grupo_obstaculos = pygame.sprite.Group()
 
-# Criar chão
+# Criar chão e céu
 chao = Chao(y_pos=510, vel=5)
-
-#Criar o céu
 ceu = Ceu(vel=2, largura_tela=LARGURA)
+
+
+SPAWN_OBSTACULO = pygame.USEREVENT + 1
+pygame.time.set_timer(SPAWN_OBSTACULO, 3500) #cada milisegundos acontece
+
+
+def checar_colisao():
+    if pygame.sprite.spritecollide(grupo_jogador.sprite, grupo_obstaculos, False, pygame.sprite.collide_mask):
+        return True 
+    return False 
 
 # Loop principal
 rodando = True
@@ -29,19 +39,28 @@ while rodando:
         if event.type == pygame.QUIT:
             rodando = False
 
+        if event.type == SPAWN_OBSTACULO:
+            grupo_obstaculos.add(Obstaculo(vel=5, largura_tela=LARGURA, altura_chao=510))
+
+    TELA.fill((135, 206, 235))  
     keys = pygame.key.get_pressed()
-    grupo.update(keys)
+    grupo_jogador.update(keys)
     chao.update()
     ceu.update()
+    grupo_obstaculos.update() # 
 
     # Desenho
-    TELA.fill((135, 206, 235))  # Céu
     ceu.draw(TELA)
     chao.draw(TELA)
-    grupo.draw(TELA)
-    
+    grupo_jogador.draw(TELA)
+    grupo_obstaculos.draw(TELA) 
+
+ 
+    if checar_colisao():
+        rodando = False 
 
     pygame.display.flip()
     clock.tick(60)
 
 pygame.quit()
+sys.exit()
